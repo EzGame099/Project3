85b48b2851918ba5777b61f0c181a4d2901b741d
Arbim:) <70119476+EzGame099@users.noreply.github.com>
2022-02-26 13:15:51 +0700

    Testing ke4 memakai git

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,6 +69,7 @@
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                     Coba-coba
+                                    Kami kelompok 8
                                 </div>
                             </div>
                         </div>
